802c3fc921e5ef7f9d566ccbc9fad81f0ae9a13d
steven <steven.bucaille@gmail.com>
2023-02-22 21:32:51 +0100

    Added dataset implementation

--- src/train.py
+++ src/train.py
@@ -1,10 +1,16 @@
 from omegaconf import DictConfig, OmegaConf
 import hydra
 
+from torch.utils.data import Dataset, DataLoader
+
 @hydra.main(version_base=None, config_path="../conf", config_name="train.yaml")
 def my_app(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
+    dataset : Dataset = hydra.utils.instantiate(cfg.data)
+    print(dataset)
+    dataloader : DataLoader = hydra.utils.instantiate(dataset, cfg.dataloader)
+    print(dataloader)
 
 if __name__ == "__main__":
     my_app()
