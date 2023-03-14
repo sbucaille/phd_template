3cb86372d1e8b79a6cb40b02a330f79cf51f0f95
Steven Bucaille <steven.bucaille@buawei.com>
2023-03-14 11:04:57 +0100

    fix: - removed useless imports - handled dynamic shapes in provider

--- src/onnx_to_tensorrt.py
+++ src/onnx_to_tensorrt.py
@@ -2,8 +2,8 @@ import hydra
 import numpy as np
 from omegaconf import DictConfig, OmegaConf
 
-from cuda import cuda
-import pycuda.autoinit
+# from cuda import cuda
+# import pycuda.autoinit
 import tensorrt as trt
 
 
@@ -16,7 +16,6 @@ def my_app(cfg: DictConfig) -> None:
     network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
     profile = builder.create_optimization_profile()
     config = builder.create_builder_config()
-
 
     if cfg.engine_precision == 'FP16':
         config.set_flag(trt.BuilderFlag.FP16)
@@ -32,18 +31,29 @@ def my_app(cfg: DictConfig) -> None:
         print("Succeeded parsing .onnx file!")
 
     input = network.get_input(0)
+    print(input.name)
 
+    if cfg.batch_size is not None:
+        profile_shape_min = (cfg.batch_size.min, cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
+        profile_shape_opt = (cfg.batch_size.opt, cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
+        profile_shape_max = (cfg.batch_size.max, cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
+    else:
+        profile_shape_min = (cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
+        profile_shape_opt = (cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
+        profile_shape_max = (cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
+
+    print(profile_shape_opt)
     profile.set_shape(input.name,
-                      (cfg.batch_size.min, cfg.input_size[0], cfg.input_size[1], cfg.input_size[2]),
-                      (cfg.batch_size.opt, cfg.input_size[0], cfg.input_size[1], cfg.input_size[2]),
-                      (cfg.batch_size.max, cfg.input_size[0], cfg.input_size[1], cfg.input_size[2])
-                      )
+                      profile_shape_min,
+                      profile_shape_opt,
+                      profile_shape_max)
     config.add_optimization_profile(profile)
 
     engine = builder.build_serialized_network(network, config)
     with open(cfg.tensorrt_path, 'wb') as f:
         f.write(engine)
     print("TensorRT engine created")
+
 
 if __name__ == "__main__":
     my_app()
